f179ba75821d81fe1ab6d228e0c594a6a119b6cc
sagemoon1996 <angemimi17@gmail.com>
2026-09-22 19:27:54 +0100

    Add build configuration for Arab Runners

---
+++ ArabRunners/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Arab Runners Team provider"
+    authors = listOf("sagemoon1996")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries"
+    )
+    language = "ar"
+}
