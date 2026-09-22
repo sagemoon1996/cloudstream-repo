b1023c7ea70f2ec145af9b3a3ee4311a35382f16
sagemoon1996 <angemimi17@gmail.com>
2026-09-22 20:27:04 +0100

    Add new imports and enhance episode loading logic

--- ArabRunners/src/main/kotlin/com/sagemoon1996/arabRunners/ArabRunnersProvider.kt
+++ ArabRunners/src/main/kotlin/com/sagemoon1996/arabRunners/ArabRunnersProvider.kt
@@ -1,7 +1,22 @@
 package com.sagemoon1996.arabRunners
 
-import com.lagradost.cloudstream3.*
+import android.content.Context
+import com.lagradost.cloudstream3.ExtractorLink
+import com.lagradost.cloudstream3.ExtractorLinkType
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.newEpisode
+import com.lagradost.cloudstream3.newExtractorLink
+import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.newTvSeriesSearchResponse
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 import java.net.URLEncoder
@@ -237,6 +252,7 @@ class ArabRunnersProvider : MainAPI() {
                         ?: return@mapNotNull null
 
                 newEpisode(episodeUrl) {
+
                     name = text.ifBlank {
                         "Episode $episode"
                     }
@@ -334,7 +350,9 @@ class ArabRunnersProvider : MainAPI() {
             .distinct()
 
         for (link in iframeLinks) {
+
             try {
+
                 loadExtractor(
                     link,
                     data,
@@ -343,6 +361,7 @@ class ArabRunnersProvider : MainAPI() {
                 )
 
                 loaded = true
+
             } catch (_: Exception) {
             }
         }
