08e01dd71482c785963ff65ea49b27a4bd88b88d
sagemoon1996 <angemimi17@gmail.com>
2026-09-22 01:22:15 +0100

    Add build.gradle.kts for project configuration

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.13.2")
+        classpath("com.github.recloudstream:gradle:81b1d424d2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "https://github.com/sagemoon1996/cloudstream-repo"
+        )
+
+        authors = listOf("sagemoon1996")
+    }
+
+    android {
+        namespace = "com.sagemoon1996"
+
+        defaultConfig {
+            minSdk = 21
+            compileSdkVersion(35)
+            targetSdk = 35
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
+    dependencies {
+        cloudstream("com.lagradost:cloudstream3:pre-release")
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.18")
+        implementation("org.jsoup:jsoup:1.22.2")
+    }
+}
