60b6737302ffeaea84ad7efd54719e6881029c20
sagemoon1996 <angemimi17@gmail.com>
2026-09-22 01:57:23 +0100

    Add build configuration for FiveTV streaming provider

---
+++ FiveTV/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 1
+
+cloudstream {
+    description = "FiveTV Arabic streaming provider"
+    authors = listOf("sagemoon1996")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie"
+    )
+    iconUrl = "https://new61.5tv.lol/wp-content/uploads/2026/09/yjPlfO23INYLyMWr1yr7rhwJhKO.jpg"
+    language = "ar"
+}
