a3798999634d152d44138b454d91d5a1f8acb4f3
sagemoon1996 <angemimi17@gmail.com>
2026-09-25 14:53:11 +0100

    Create build.gradle.kts

---
+++ AsiaShow/build.gradle.kts
@@ -0,0 +1,12 @@
+version = 1
+
+cloudstream {
+    description = "AsiaShow Arabic streaming provider"
+    authors = listOf("sagemoon1996")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie"
+    )
+    language = "ar"
+}
