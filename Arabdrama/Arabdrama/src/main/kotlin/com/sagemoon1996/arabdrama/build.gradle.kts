dc6ad46b59e50a5633ca273710b353fb9bcfe0ec
sagemoon1996 <angemimi17@gmail.com>
2026-09-26 19:13:09 +0100

    Delete Arabdrama directory

--- Arabdrama/Arabdrama/src/main/kotlin/com/sagemoon1996/arabdrama/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("com.lagradost.cloudstream3.gradle")
-}
-
-android {
-    namespace = "com.sagemoon1996.arabdrama"
-}
