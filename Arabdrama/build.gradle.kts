dba6cea20950b74722aa1eb02a35958e81bc9660
sagemoon1996 <angemimi17@gmail.com>
2026-09-26 19:16:02 +0100

    Add initial build configuration for Arabdrama

---
+++ Arabdrama/build.gradle.kts
@@ -0,0 +1,12 @@
+version = 1
+
+cloudstream {
+    description = "Arabdrama Arabic streaming provider"
+    authors = listOf("sagemoon1996")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie"
+    )
+    language = "ar"
+}
